501c528802d28a5ca14213dcbd5381caaac53235
faizalhaziq11 <faizalhaziq1189@gmail.com>
2025-07-23 00:12:14 +0800

    Update rsbuild config, .gitignore, and api

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -8,6 +8,20 @@ export default defineConfig({
   },
   server: {
     base: '/foo',
+    proxy: {
+      '/weather-api':
+      {
+        target:
+          'https://api.weatherapi.com/v1',
+        changeOrigin: true,
+
+      },
+      '/foo/external': {
+        target: 'https://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        pathRewrite: { '^/foo/external': '' },
+      },
+    }
   },
   output: {
     distPath: {
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,2 +1,3 @@
-export const WEATHER_API_URL = "https://api.weatherapi.com/v1";
+// export const WEATHER_API_URL = "https://api.weatherapi.com/v1";
+export const WEATHER_API_URL = "/weather-api";
 export const API_KEY = "8318c1ded0c14a578ab142801220108";

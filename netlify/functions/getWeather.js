7b0d5b25303ec0e48f1b0ad5d8ef150300afdbb7
faizalhaziq11 <faizalhaziq1189@gmail.com>
2025-12-17 17:26:13 +0800

    feat: add Netlify configuration

---
+++ netlify/functions/getWeather.js
@@ -0,0 +1,28 @@
+import { apiClient } from "../../src/helper/requestHelper";
+
+const baseUrl = import.meta.env.MODE === 'production' ? import.meta.env.PUBLIC_WEATHER_API_URL : '/weather-api';
+const apiKey = import.meta.env.PUBLIC_WEATHER_API_KEY;
+
+export const getCurrentWeather = (location = "") => {
+  const sanitizedLocation = encodeURIComponent(location.trim());
+  const params = new URLSearchParams({
+    key: apiKey,
+    q: sanitizedLocation,
+    aqi: "no"
+  });
+
+  return apiClient.get(baseUrl + '/current.json?', params)
+}
+
+export const getForecastWeather = (location = "") => {
+  const sanitizedLocation = encodeURIComponent(location.trim());
+  const params = new URLSearchParams({
+    key: apiKey,
+    q: sanitizedLocation,
+    days: 10,
+    aqi: "no",
+    alerts: "no"
+  });
+
+  return apiClient.get(baseUrl + '/forecast.json?', params)
+}
